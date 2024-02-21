34542fe44032a6183951293e14b1fccf6e39d646
alexandra toloza rios <toloza.ale1992@gmail.com>
2024-02-21 19:52:31 -0300

    guardando cambios

--- assets/jvs/app.js
+++
@@ -1,8 +0,0 @@
-
-
-console.log ("Activaremos el boton")
-
-let BotonparaEnviar = document.querySelector("#BotonparaEnviar")
-BotonparaEnviar.addEventListener(button, function(BotonparaEnviar){
-    console.log ("Activar")
-})

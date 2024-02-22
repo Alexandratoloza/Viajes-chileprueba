9a40c9fec11a57fac74abd111409880633059bd9
alexandra toloza rios <toloza.ale1992@gmail.com>
2024-02-22 11:44:37 -0300

    cambios 1

---
+++ assets/jvs/app.js
@@ -0,0 +1,8 @@
+ let BotonparaEnviar = document.querySelectorall("#BotonparaEnviar")
+ let btn = document.querySelector("button");
+ btn.addEventListener("click", function(){
+    nombre.style,color = "blue";
+ })
+ 
+ 
+ 
